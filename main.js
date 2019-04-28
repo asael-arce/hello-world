beff4b4a92db9729abe6a988efcb5cbb54a4e4c6
asael-arce <asael-arce@hotmail.com>
2019-04-28 11:20:01 -0400

    update

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function save(){
-    var texto = document.getElementById('ejem').value;
-    prompt(texto);
+
+function myFunction(){
+    var nombre = document.getElementById('ejem').value;
+    var email = document.getElementById('ejem1').value;
+    var tema = document.getElementById('ejem2').value;
+    var comentario = document.getElementById('ejem3').value;
+    alert(nombre + email + tema + comentario);
 }
+
+
 
